1bb94078074887b5c438008460e990a5c6c2aec9
OrestKupyn <orestkupyn@gmail.com>
2018-10-31 18:51:23 +0200

    fpn changed

--- fpn.py
+++ fpn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from torchvision.models import resnet50
+from torchvision.models import resnet50, densenet121, densenet201
 
 
 class FPNSegHead(nn.Module):
@@ -19,7 +19,7 @@ class FPNSegHead(nn.Module):
 
 class FPNSeg(nn.Module):
 
-    def __init__(self, num_classes, num_filters=128, num_filters_fpn=256, pretrained=True):
+    def __init__(self, output_ch=3, num_filters=128, num_filters_fpn=256, pretrained=True):
         super().__init__()
 
         # Feature Pyramid Network (FPN) with four feature maps of resolutions
@@ -34,21 +34,37 @@ class FPNSeg(nn.Module):
         self.head3 = FPNSegHead(num_filters_fpn, num_filters, num_filters)
         self.head4 = FPNSegHead(num_filters_fpn, num_filters, num_filters)
 
-        self.final = nn.Conv2d(4 * num_filters, num_classes, kernel_size=3, padding=1)
+        self.smooth = nn.Sequential(
+            nn.Conv2d(4 * num_filters, num_filters, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_filters),
+            nn.ReLU(),
+        )
+
+        self.smooth2 = nn.Sequential(
+            nn.Conv2d(num_filters, num_filters // 2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_filters // 2),
+            nn.ReLU(),
+        )
+
+        self.final = nn.Conv2d(num_filters // 2, output_ch, kernel_size=3, padding=1)
 
     def forward(self, x):
-        map1, map2, map3, map4 = self.fpn(x)
+
+        map0, map1, map2, map3, map4 = self.fpn(x)
 
         map4 = nn.functional.upsample(self.head4(map4), scale_factor=8, mode="nearest")
         map3 = nn.functional.upsample(self.head3(map3), scale_factor=4, mode="nearest")
         map2 = nn.functional.upsample(self.head2(map2), scale_factor=2, mode="nearest")
-        map1 = self.head1(map1)
+        map1 = nn.functional.upsample(self.head1(map1), scale_factor=1, mode="nearest")
 
-        final = self.final(torch.cat([map4, map3, map2, map1], dim=1))
+        smoothed = self.smooth(torch.cat([map4, map3, map2, map1], dim=1))
+        smoothed = nn.functional.upsample(smoothed , scale_factor=2, mode="nearest")
+        smoothed = self.smooth2(smoothed + map0)
+        smoothed = nn.functional.upsample(smoothed, scale_factor=2, mode="nearest")
 
-        output = nn.functional.upsample(final, scale_factor=4, mode="nearest")
+        final = self.final(smoothed)
 
-        return output
+        nn.Tanh(final)
 
 
 class FPN(nn.Module):
@@ -62,34 +78,48 @@ class FPN(nn.Module):
 
         super().__init__()
 
-        self.resnet = resnet50(pretrained=pretrained)
+        self.features = densenet121(pretrained=pretrained).features
 
-        self.enc0 = nn.Sequential(self.resnet.conv1, self.resnet.bn1, self.resnet.relu, self.resnet.maxpool)
-        self.enc1 = self.resnet.layer1  # 256
-        self.enc2 = self.resnet.layer2  # 512
-        self.enc3 = self.resnet.layer3  # 1024
-        self.enc4 = self.resnet.layer4  # 2048
+        self.enc0 = nn.Sequential(self.features.conv0,
+                                  self.features.norm0,
+                                  self.features.relu0)
+        self.pool0 = self.features.pool0
+        self.enc1 = self.features.denseblock1  # 256
+        self.enc2 = self.features.denseblock2  # 512
+        self.enc3 = self.features.denseblock3  # 1024
+        self.enc4 = self.features.denseblock4  # 2048
+        self.norm = self.features.norm5  # 2048
 
-        self.lateral4 = nn.Conv2d(2048, num_filters, kernel_size=1, bias=False)
+        self.tr1 = self.features.transition1  # 256
+        self.tr2 = self.features.transition2  # 512
+        self.tr3 = self.features.transition3  # 1024
+
+        self.lateral4 = nn.Conv2d(1024, num_filters, kernel_size=1, bias=False)
         self.lateral3 = nn.Conv2d(1024, num_filters, kernel_size=1, bias=False)
         self.lateral2 = nn.Conv2d(512, num_filters, kernel_size=1, bias=False)
         self.lateral1 = nn.Conv2d(256, num_filters, kernel_size=1, bias=False)
+        self.lateral0 = nn.Conv2d(64, num_filters // 2, kernel_size=1, bias=False)
 
-        self.smooth4 = nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1, bias=False)
-        self.smooth3 = nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1, bias=False)
-        self.smooth2 = nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1, bias=False)
-        self.smooth1 = nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1, bias=False)
 
 
     def forward(self, x):
 
         # Bottom-up pathway, from ResNet
-
         enc0 = self.enc0(x)
-        enc1 = self.enc1(enc0) # 256
-        enc2 = self.enc2(enc1) # 512
-        enc3 = self.enc3(enc2) # 1024
-        enc4 = self.enc4(enc3) # 2048
+
+        pooled = self.pool0(enc0)
+
+        enc1 = self.enc1(pooled) # 256
+        tr1 = self.tr1(enc1)
+
+        enc2 = self.enc2(tr1) # 512
+        tr2 = self.tr2(enc2)
+
+        enc3 = self.enc3(tr2) # 1024
+        tr3 = self.tr3(enc3)
+
+        enc4 = self.enc4(tr3) # 2048
+        enc4 = self.norm(enc4)
 
         # Lateral connections
 
@@ -97,6 +127,7 @@ class FPN(nn.Module):
         lateral3 = self.lateral3(enc3)
         lateral2 = self.lateral2(enc2)
         lateral1 = self.lateral1(enc1)
+        lateral0 = self.lateral0(enc0)
 
         # Top-down pathway
 
@@ -105,11 +136,4 @@ class FPN(nn.Module):
         map2 = lateral2 + nn.functional.upsample(map3, scale_factor=2, mode="nearest")
         map1 = lateral1 + nn.functional.upsample(map2, scale_factor=2, mode="nearest")
 
-        # Reduce aliasing effect of upsampling
-
-        map4 = self.smooth4(map4)
-        map3 = self.smooth3(map3)
-        map2 = self.smooth2(map2)
-        map1 = self.smooth1(map1)
-
-        return map1, map2, map3, map4
+        return lateral0, map1, map2, map3, map4

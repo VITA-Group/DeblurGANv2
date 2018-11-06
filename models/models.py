c88202801542fba988103052de6648b4f225dde3
KupynOrest <orestkupyn@gmail.com>
2018-11-06 12:20:55 +0200

    towards flexible pipeline

--- models/models.py
+++ models/models.py
@@ -1,13 +1,40 @@
-from .conditional_gan_model import ConditionalGAN
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import torchvision.models as models
+from torch.autograd import Variable
+import torchvision.utils as vutils
+import torchvision.transforms as transforms
+import numpy as np
 
-def create_model(opt):
-	model = None
-	if opt.model == 'test':
-		assert (opt.dataset_mode == 'single')
-		from .test_model import TestModel
-		model = TestModel()
-	else:
-		model = ConditionalGAN()
-	model.initialize(opt)
-	print("model [%s] was created" % (model.name()))
-	return model
+
+class DeblurModel(nn.Module):
+    def __init__(self):
+        super(DeblurModel, self).__init__()
+
+    def get_input(self, data):
+        img = data['A']
+        inputs = img
+        targets = data['B']
+        inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        return inputs, targets
+
+    def get_acc(self, output=None, target=None):
+        return 0
+
+    def get_loss(self, mean_loss, mean_acc, output=None, target=None):
+        return '{:.3f}; accuracy={}'.format(mean_loss, mean_acc)
+
+    def visualize_data(self, writer, data, outputs, niter):
+        inv_normalize = transforms.Normalize(
+            mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.255],
+            std=[1 / 0.229, 1 / 0.224, 1 / 0.255]
+        )
+        images = inv_normalize(vutils.make_grid(data['A']))
+        writer.add_image('Images', images, niter)
+
+
+def get_model(model_config):
+    return DeblurModel()
+
---
+++ schedulers.py
@@ -0,0 +1,35 @@
+from torch.optim import lr_scheduler
+import math
+
+class WarmRestart(lr_scheduler.CosineAnnealingLR):
+    """This class implements Stochastic Gradient Descent with Warm Restarts(SGDR): https://arxiv.org/abs/1608.03983.
+
+    Set the learning rate of each parameter group using a cosine annealing schedule, When last_epoch=-1, sets initial lr as lr.
+    This can't support scheduler.step(epoch). please keep epoch=None.
+    """
+
+    def __init__(self, optimizer, T_max=30, T_mult=1, eta_min=0, last_epoch=-1):
+        """implements SGDR
+
+        Parameters:
+        ----------
+        T_max : int
+            Maximum number of epochs.
+        T_mult : int
+            Multiplicative factor of T_max.
+        eta_min : int
+            Minimum learning rate. Default: 0.
+        last_epoch : int
+            The index of last epoch. Default: -1.
+        """
+        self.T_mult = T_mult
+        super().__init__(optimizer, T_max, eta_min, last_epoch)
+
+    def get_lr(self):
+        print(self.last_epoch)
+        if self.last_epoch == self.T_max:
+            self.last_epoch = 0
+            self.T_max *= self.T_mult
+        return [self.eta_min + (base_lr - self.eta_min) * (1 + math.cos(math.pi * self.last_epoch / self.T_max)) / 2 for
+                base_lr in self.base_lrs]
+

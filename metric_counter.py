77f3cc58d5c7e8cba39247269fbd46f18ec54477
KupynOrest <orestkupyn@gmail.com>
2019-02-04 16:06:15 +0200

    refactored metrics counter
    # Conflicts:
    #	metric_counter.py

--- metric_counter.py
+++ metric_counter.py
@@ -1,56 +1,59 @@
-import logging
-from collections import defaultdict
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 import numpy as np
 from tensorboardX import SummaryWriter
+import logging
 
-WINDOW_SIZE = 100
+REPORT_EACH = 100
 
-
-class MetricCounter:
+class MetricCounter():
     def __init__(self, exp_name):
+        super(MetricCounter, self).__init__()
         self.writer = SummaryWriter(exp_name)
         logging.basicConfig(filename='{}.log'.format(exp_name), level=logging.DEBUG)
-        self.metrics = defaultdict(list)
-        self.images = defaultdict(list)
+        self.clear()
         self.best_metric = 0
 
-    def add_image(self, x: np.ndarray, tag: str):
-        self.images[tag].append(x)
-
     def clear(self):
-        self.metrics = defaultdict(list)
-        self.images = defaultdict(list)
+        self.G_loss = []
+        self.D_loss = []
+        self.content_loss = []
+        self.adv_loss = []
+        self.psnr = []
+        self.ssim = []
 
-    def add_losses(self, l_G, l_content, l_D=0):
-        for name, value in zip(('G_loss', 'G_loss_content', 'G_loss_adv', 'D_loss'),
-                               (l_G, l_content, l_G - l_content, l_D)):
-            self.metrics[name].append(value)
+    def add_losses(self, l_G, l_D, l_content, l_adv):
+        self.G_loss.append(l_G)
+        self.content_loss.append(l_content)
+        self.adv_loss.append(l_adv)
+        self.D_loss.append(l_D)
 
     def add_metrics(self, psnr, ssim):
-        for name, value in zip(('PSNR', 'SSIM'),
-                               (psnr, ssim)):
-            self.metrics[name].append(value)
+        self.psnr.append(psnr)
+        self.ssim.append(ssim)
 
     def loss_message(self):
-        metrics = ((k, np.mean(self.metrics[k][-WINDOW_SIZE:])) for k in ('G_loss', 'PSNR', 'SSIM'))
-        return '; '.join(map(lambda x: f'{x[0]}={x[1]:.4f}', metrics))
+        mean_loss = np.mean(self.G_loss[-REPORT_EACH:])
+        mean_psnr = np.mean(self.psnr[-REPORT_EACH:])
+        mean_ssim = np.mean(self.ssim[-REPORT_EACH:])
+        return '{:.3f}; psnr={}; ssim={}'.format(mean_loss, mean_psnr, mean_ssim)
 
     def write_to_tensorboard(self, epoch_num, validation=False):
         scalar_prefix = 'Validation' if validation else 'Train'
-        for tag in ('G_loss', 'D_loss', 'G_loss_adv', 'G_loss_content', 'SSIM', 'PSNR'):
-            self.writer.add_scalar(f'{scalar_prefix}_{tag}', np.mean(self.metrics[tag]), global_step=epoch_num)
-        for tag in self.images:
-            imgs = self.images[tag]
-            if imgs:
-                imgs = np.array(imgs)
-                self.writer.add_images(tag, imgs[:, :, :, ::-1].astype('float32') / 255, dataformats='NHWC',
-                                       global_step=epoch_num)
-                self.images[tag] = []
+        writer.add_scalar('{}_G_Loss'.format(scalar_prefix), np.mean(self.G_loss), epoch_num)
+        writer.add_scalar('{}_D_Loss'.format(scalar_prefix), np.mean(self.D_loss), epoch_num)
+        writer.add_scalar('{}_G_Loss_content'.format(scalar_prefix), np.mean(self.content_loss), epoch_num)
+        writer.add_scalar('{}_G_Loss_adv'.format(scalar_prefix), np.mean(self.adv_loss), epoch_num)
+        writer.add_scalar('{}_SSIM'.format(scalar_prefix), np.mean(self.psnr), epoch_num)
+        writer.add_scalar('{}_PSNR'.format(scalar_prefix), np.mean(self.ssim), epoch_num)
 
     def update_best_model(self):
-        cur_metric = np.mean(self.metrics['PSNR'])
+        cur_metric = np.mean(self.psnr)
         if self.best_metric < cur_metric:
             self.best_metric = cur_metric
             return True
         return False
+
+

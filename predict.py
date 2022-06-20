e39990136e7c087ad744ae6831e916148557787c
ChaoWANG <nantes1967@gmail.com>
2022-06-20 14:11:40 +0800

    新加图片，更改了main函数的读入图片方式，增加了.h5模型，更改了模型读入参数

--- predict.py
+++ predict.py
@@ -90,7 +90,7 @@ def process_video(pairs, predictor, output_dir):
 
 def main(img_pattern: str,
          mask_pattern: Optional[str] = None,
-         weights_path='best_fpn.h5',
+         weights_path='fpn_inception.h5',
          out_dir='submit/',
          side_by_side: bool = False,
          video: bool = False):
@@ -121,4 +121,5 @@ def main(img_pattern: str,
 
 
 if __name__ == '__main__':
-    Fire(main)
+  #  Fire(main)
+    main('submit/t6.jpg')
